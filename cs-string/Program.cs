9107aca2d52224d84334962fa89dabf397b25f0a
Subhajit Sahu <wolfram77@gmail.com>
2016-06-07 21:39:01 +0530

    program: this a crysis but i will face it

--- cs-string/Program.cs
+++ cs-string/Program.cs
@@ -9,5 +9,13 @@ namespace orez.ostring {
 		/// <param name="args">Input parameters.</param>
 		static void Main(string[] args) {
 		}
+
+		private static string Run(string s, string[] p) {
+			if(p.Length > 0) switch(p[0]) {
+					case "size":
+						return s.Length.ToString();
+			}
+			return null;
+		}
 	}
 }
