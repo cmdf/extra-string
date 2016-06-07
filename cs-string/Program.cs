65fae89aa2028c38f1274dd7f100da086a2db504
Subhajit Sahu <wolfram77@gmail.com>
2016-06-07 22:20:57 +0530

    program: string reverse function added

--- cs-string/Program.cs
+++ cs-string/Program.cs
@@ -8,14 +8,26 @@ namespace orez.ostring {
 		/// </summary>
 		/// <param name="args">Input parameters.</param>
 		static void Main(string[] args) {
+			string s = "abcd";
+			s = s.Substring(3, 0);
 		}
 
 		private static string Run(string s, string[] p) {
 			if(p.Length > 0) switch(p[0]) {
+					case "at":
+						return null;
+					case "compare":
+						return s.CompareTo(p.Length > 1 ? p[1] : "").ToString();
 					case "size":
 						return s.Length.ToString();
 			}
 			return null;
+		}
+
+		private static string Reverse(string s) {
+			char[] c = s.ToCharArray();
+			Array.Reverse(c);
+			return new string(c);
 		}
 	}
 }
