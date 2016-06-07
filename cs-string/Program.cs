ce75649873c8d91317ddeb0d62a8558065b31e59
Subhajit Sahu <wolfram77@gmail.com>
2016-06-07 21:36:18 +0530

    program: cool comments added to the rangers

--- cs-string/Program.cs
+++ cs-string/Program.cs
@@ -2,6 +2,11 @@
 
 namespace orez.ostring {
 	class Program {
+
+		/// <summary>
+		/// Smack that. Oooo.
+		/// </summary>
+		/// <param name="args">Input parameters.</param>
 		static void Main(string[] args) {
 		}
 	}
