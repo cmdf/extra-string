a4a5b8fc0d784122f6c48e2468799b9894b85852
Subhajit Sahu <wolfram77@gmail.com>
2016-06-10 02:55:10 +0530

    oparams: regex flags moved below

--- cs-string/oParams.cs
+++ cs-string/oParams.cs
@@ -2,10 +2,6 @@
 	class oParams {
 		
 		/// <summary>
-		/// Indicates whether specified search pattern is RegEx.
-		/// </summary>
-		public bool regex = false;
-		/// <summary>
 		/// Tells the string function to be used.
 		/// </summary>
 		public string fn = "";
@@ -13,5 +9,9 @@
 		/// Defines input arguments to specified function.
 		/// </summary>
 		public string[] args = new string[0];
+		/// <summary>
+		/// Indicates whether specified search pattern is RegEx.
+		/// </summary>
+		public bool regex = false;
 	}
 }
