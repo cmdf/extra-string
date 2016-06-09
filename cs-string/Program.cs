8b3b9923a160e40129fe7544904d19cf8b883314
Subhajit Sahu <wolfram77@gmail.com>
2016-06-09 18:05:35 +0530

    program: need a flag for indicating regex search pattern

--- cs-string/Program.cs
+++ cs-string/Program.cs
@@ -246,5 +246,6 @@ namespace orez.ostring {
 		// isint isdecimal -> can be implemented with regex equals
 		// line -> generic line find and replace?
 		// multi-line operate, input stream operate
+		// --regex|-r flag for regex search pattern
 	}
 }
