8876fb01dcd15b6a2e1040a53dfa50909388b1d7
Subhajit Sahu <wolfram77@gmail.com>
2016-06-11 01:51:28 +0530

    ostringext: regexopt added

--- cs-string/data/oStringExt.cs
+++ cs-string/data/oStringExt.cs
@@ -1,9 +1,33 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace orez.ostring.data {
 	static class oStringExt {
 
+		// data
+		/// <summary>
+		/// Regex options associated with characters.
+		/// </summary>
+		private static IDictionary<char, RegexOptions> ReOpt = new Dictionary<char, RegexOptions> {
+			['i'] = RegexOptions.IgnoreCase, ['m'] = RegexOptions.Multiline, ['r'] = RegexOptions.RightToLeft,
+			['s'] = RegexOptions.Singleline
+		};
+
+
 		// extension methods
+		/// <summary>
+		/// Get Regex options from options string.
+		/// </summary>
+		/// <param name="s">Options string.</param>
+		/// <returns>Regex options.</returns>
+		public static RegexOptions RegExOpt(string s) {
+			RegexOptions o = RegexOptions.None;
+			for (int i = 0; i < s.Length; i++)
+				if (ReOpt.ContainsKey(s[i])) o |= ReOpt[s[i]];
+			return o;
+		}
+
 		/// <summary>
 		/// Repeat a string certain number of times.
 		/// </summary>
