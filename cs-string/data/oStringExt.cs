247cb44f55300c1c0b96b2e5fa283d1aef8aa796
Subhajit Sahu <wolfram77@gmail.com>
2016-06-11 01:48:33 +0530

    project: string extension class added with repeat fn

---
+++ cs-string/data/oStringExt.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace orez.ostring.data {
+	static class oStringExt {
+
+		// extension methods
+		/// <summary>
+		/// Repeat a string certain number of times.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="n">Times.</param>
+		/// <returns>Repeated string.</returns>
+		public static string Repeat(this string s, int n) {
+			StringBuilder o = new StringBuilder();
+			for (int i = 0; i < n; i++)
+				o.Append(s);
+			return o.ToString();
+		}
+	}
+}
