77784eb93743a00da2778f6c1c01d811b8dea5c3
Subhajit Sahu <wolfram77@gmail.com>
2016-06-10 17:38:12 +0530

    program: rearranged and updated functions

--- cs-string/Program.cs
+++ cs-string/Program.cs
@@ -23,11 +23,11 @@ namespace orez.ostring {
 		/// Associates function name with the actual function.
 		/// </summary>
 		private static IDictionary<string, Fn> StrFn = new Dictionary<string, Fn> {
-			{"add",  new Fn(Add)}, {"get", new Fn(Get)}, {"compare", new Fn(Compare)}, {"copy", new Fn(Copy)},
-			{ "endswith", new Fn(EndsWith)}, {"find",  new Fn(Find)}, {"format", new Fn(Format)}, {"lowercase", new Fn(LowerCase)},
-			{ "put", new Fn(Put)}, {"range", new Fn(Range)}, { "remove", new Fn(Remove)}, { "replace", new Fn(Replace)},
-			{"reverse", new Fn(Reverse)}, {"size", new Fn(Size)}, {"startswith", new Fn(StartsWith)}, {"uppercase", new Fn(UpperCase)},
-			{"encode", new Fn(Encode)}, {"decode", new Fn(Decode)}, {"code", new Fn(Code)}
+			["size"] = new Fn(Size), ["get"] = new Fn(Get), ["range"] = new Fn(Range),
+			["find"] = new Fn(Find), ["compare"] = new Fn(Compare), ["startswith"] = new Fn(StartsWith), ["endswith"] = new Fn(EndsWith),
+			["code"] = new Fn(Code), ["encode"] = new Fn(Encode), ["decode"] = new Fn(Decode),
+			["copy"] = new Fn(Copy), ["format"] = new Fn(Format), ["add"] = new Fn(Add), ["put"] = new Fn(Put),
+			["replace"] = new Fn(Replace), ["remove"] = new Fn(Remove), ["reverse"] = new Fn(Reverse)
 		};
 		/// <summary>
 		/// String Encode table for DOS without delayed expansion enabled.
@@ -103,139 +103,160 @@ namespace orez.ostring {
 		}
 
 		/// <summary>
-		/// Add a string to input string.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">add, index.</param>
-		/// <returns>Added string.</returns>
-		private static void Add(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			int i = Indx(Int(p, 1, s.Length), s);
-			Console.WriteLine(s.Insert(i, t));
-		}
-		
-		/// <summary>
-		/// Get character codes for each character in the string.
+		/// Get the size of a string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">NA.</param>
-		/// <param name="re">Is input search string regex?</param>
-		private static void Code(string s, string[] p, bool re) {
-			for (int i = 0; i < s.Length; i++)
-				Console.WriteLine((uint)s[i]);
-		}
-		/// <summary>
-		/// Decode or Unescape string to original form.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">type.</param>
-		/// <returns>Decoded string.</returns>
-		/// <returns></returns>
-		private static void Decode(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			Console.WriteLine(Decode(s, t));
+		/// <param name="re">NA.</param>
+		private static void Size(string s, string[] p, bool re) {
+			Console.WriteLine(s.Length);
 		}
 		/// <summary>
 		/// Get a part of input string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">index, length.</param>
-		/// <returns>Part of string.</returns>
+		/// <param name="re">NA.</param>
 		private static void Get(string s, string[] p, bool re) {
 			int i = Indx(Int(p, 0), s);
 			int e = Indx(i + Int(p, 1, 1), s);
 			Console.WriteLine(s.Substring(i, e - i));
 		}
 		/// <summary>
-		/// Compare two strings.
-		/// </summary>
-		/// <param name="s">1st input string.</param>
-		/// <param name="t">2nd input string.</param>
-		/// <returns>
-		/// A number that is:
-		/// &gt; 0 - if 1st string comes after 2nd string.
-		///   == 0 - if both the strings are equal.
-		/// &lt; 0 - if 1st string comes before 2nd string.
-		/// </returns>
-		private static void Compare(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			if(!re) { Console.WriteLine(s.CompareTo(t)); return; }
-			Console.WriteLine(RegEx(t).Match(s).Length == s.Length ? "0" : "-1");
-		}
-		/// <summary>
-		/// Copies input string specified number of times.
+		/// Get a specified range of input string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
-		/// <param name="p">times.</param>
-		/// <returns>Copied string.</returns>
-		private static void Copy(string s, string[] p, bool re) {
-			StringBuilder t = new StringBuilder();
-			int n = Math.Abs(Int(p, 0));
-      for(int i = 0; i < n; i++)
-				t.Append(s);
-			Console.WriteLine(t);
-		}
-		/// <summary>
-		/// Check whether input string ends with suffix.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">suffix.</param>
-		/// <returns>1 if true, 0 otherwise.</returns>
-		private static void EndsWith(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			if(!re) Console.WriteLine(s.EndsWith(t) ? "1" : "0");
-			Match m = RegEx(t, RegexOptions.RightToLeft).Match(s);
-			Console.WriteLine(m.Length == t.Length ? "1" : "0");
-		}
-		/// <summary>
-		/// Encode or Escape string to coded form.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">type.</param>
-		/// <returns>Encoded string.</returns>
-		/// <returns></returns>
-		private static void Encode(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			Console.WriteLine(Encode(s, t));
+		/// <param name="p">start, end.</param>
+		/// <param name="re">NA.</param>
+		private static void Range(string s, string[] p, bool re) {
+			int i = Indx(Int(p, 0), s);
+			int e = Indx(Int(p, 1, s.Length), s);
+			Console.WriteLine(s.Substring(i, e - i));
 		}
 		/// <summary>
 		/// Find index of string in the input string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">start, direction.</param>
-		/// <returns></returns>
+		/// <param name="re">Is search string regex?</param>
 		private static void Find(string s, string[] p, bool re) {
 			string t = Str(p, 0);
 			int i = Indx(Int(p, 1), s), d = Int(p, 1, 1);
-			if(!re) for(int n = 0, N = Math.Abs(d); n < N; n++, i = d >= 0 ? i + 1 : i - 1)
+			if (!re) for (int n = 0, N = Math.Abs(d); n < N; n++, i = d >= 0 ? i + 1 : i - 1)
 					Console.WriteLine(i = (d >= 0 ? s.IndexOf(t, i) : s.LastIndexOf(t, i)));
-			foreach(Match m in RegEx(t, d < 0 ? RegexOptions.RightToLeft : RegexOptions.None).Matches(s, i))
-				Console.WriteLine(m.Index+" "+m.Length);
+			foreach (Match m in RegEx(t, d < 0 ? RegexOptions.RightToLeft : RegexOptions.None).Matches(s, i))
+				Console.WriteLine(m.Index + " " + m.Length);
+		}
+		/// <summary>
+		/// Compare two strings.
+		/// Returns a number that is:
+		/// &gt; 0 - if 1st string comes after 2nd string.
+		///   == 0 - if both the strings are equal.
+		/// &lt; 0 - if 1st string comes before 2nd string.
+		/// </summary>
+		/// <param name="s">1st input string.</param>
+		/// <param name="t">2nd input string.</param>
+		/// <param name="re">Is search string regex?</param>
+		private static void Compare(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			if (!re) { Console.WriteLine(s.CompareTo(t)); return; }
+			Console.WriteLine(RegEx(t).Match(s).Length == s.Length ? "0" : "-1");
+		}
+		/// <summary>
+		/// Check whether input string starts with prefix.
+		/// Returns 1 if true, 0 otherwise.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">prefix.</param>
+		/// <param name="re">Is search string regex?</param>
+		private static void StartsWith(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			if (!re) Console.WriteLine(s.StartsWith(t) ? "1" : "0");
+			Match m = RegEx(t).Match(s);
+			Console.WriteLine(m.Length == t.Length ? "1" : "0");
+		}
+		/// <summary>
+		/// Check whether input string ends with suffix.
+		/// Returns 1 if true, 0 otherwise.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">suffix.</param>
+		/// <param name="re">Is search string regex?</param>
+		private static void EndsWith(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			if (!re) Console.WriteLine(s.EndsWith(t) ? "1" : "0");
+			Match m = RegEx(t, RegexOptions.RightToLeft).Match(s);
+			Console.WriteLine(m.Length == t.Length ? "1" : "0");
+		}
+		/// <summary>
+		/// Get character codes for each character in the string.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">NA.</param>
+		/// <param name="re">Is search string regex?</param>
+		private static void Code(string s, string[] p, bool re) {
+			for (int i = 0; i < s.Length; i++)
+				Console.WriteLine((uint)s[i]);
+		}
+		/// <summary>
+		/// Encode or Escape string to coded form.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">type.</param>
+		/// <param name="re">NA.</param>
+		private static void Encode(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			Console.WriteLine(Encode(s, t));
+		}
+		/// <summary>
+		/// Decode or Unescape string to original form.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">type.</param>
+		/// <param name="re">NA.</param>
+		private static void Decode(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			Console.WriteLine(Decode(s, t));
+		}
+		/// <summary>
+		/// Copies input string specified number of times.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">times.</param>
+		/// <param name="re">NA.</param>
+		private static void Copy(string s, string[] p, bool re) {
+			StringBuilder t = new StringBuilder();
+			int n = Math.Abs(Int(p, 0));
+			for (int i = 0; i < n; i++)
+				t.Append(s);
+			Console.WriteLine(t);
 		}
 		/// <summary>
 		/// Uses input string as format to embed parameter strings.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">parameters.</param>
-		/// <returns>Formatted string.</returns>
+		/// <param name="re">NA.</param>
 		private static void Format(string s, string[] p, bool re) {
 			Console.WriteLine(string.Format(s, p));
 		}
 		/// <summary>
-		/// Convert input string to lower case.
+		/// Add a string to input string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
-		/// <param name="p">NA.</param>
-		/// <returns>Lower cased string.</returns>
-		private static void LowerCase(string s, string[] p, bool re) {
-			Console.WriteLine(s.ToLower());
+		/// <param name="p">add, index.</param>
+		/// <param name="re">NA.</param>
+		private static void Add(string s, string[] p, bool re) {
+			string t = Str(p, 0);
+			int i = Indx(Int(p, 1, s.Length), s);
+			Console.WriteLine(s.Insert(i, t));
 		}
 		/// <summary>
 		/// Put a string onto input string at specified index.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">string, index.</param>
-		/// <returns>Put string.</returns>
+		/// <param name="re">NA.</param>
 		private static void Put(string s, string[] p, bool re) {
 			string t = Str(p, 0);
 			int i = Indx(Int(p, 1), s);
@@ -243,33 +264,11 @@ namespace orez.ostring {
 			Console.WriteLine(s.Remove(i, e - i).Insert(i, t));
 		}
 		/// <summary>
-		/// Get a specified range of input string.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">start, end.</param>
-		/// <returns>Part of string.</returns>
-		private static void Range(string s, string[] p, bool re) {
-			int i = Indx(Int(p, 0), s);
-			int e = Indx(Int(p, 1, s.Length), s);
-			Console.WriteLine(s.Substring(i, e - i));
-		}
-		/// <summary>
-		/// Remove part of input string.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">length, index.</param>
-		/// <returns>Removed string.</returns>
-		private static void Remove(string s, string[] p, bool re) {
-			int l = Indx(Int(p, 0), s);
-			int i = Indx(Int(p, 1, s.Length - l), s);
-			Console.WriteLine(s.Remove(i, l));
-		}
-		/// <summary>
 		/// Replace a search string with new string in input string.
 		/// </summary>
 		/// <param name="s">Input string</param>
 		/// <param name="p">search, new.</param>
-		/// <returns>Replaced string.</returns>
+		/// <param name="re">Is search string regex?</param>
 		private static void Replace(string s, string[] p, bool re) {
 			string t = Str(p, 0), u = Str(p, 1);
 			if (t == "") Console.WriteLine(string.Join(u, t.ToCharArray()));
@@ -277,36 +276,36 @@ namespace orez.ostring {
 			else Console.WriteLine(RegEx(t).Replace(s, u));
 		}
 		/// <summary>
+		/// Remove part of input string.
+		/// </summary>
+		/// <param name="s">Input string.</param>
+		/// <param name="p">length, index.</param>
+		/// <param name="re">NA.</param>
+		private static void Remove(string s, string[] p, bool re) {
+			int l = Indx(Int(p, 0), s);
+			int i = Indx(Int(p, 1, s.Length - l), s);
+			Console.WriteLine(s.Remove(i, l));
+		}
+		/// <summary>
 		/// Reverse a string.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">NA.</param>
-		/// <returns>Reversed string.</returns>
+		/// <param name="re">NA.</param>
 		private static void Reverse(string s, string[] p, bool re) {
 			char[] c = s.ToCharArray();
 			Array.Reverse(c);
 			Console.WriteLine(new string(c));
 		}
+
 		/// <summary>
-		/// Get the size of a string.
+		/// Convert input string to lower case.
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">NA.</param>
-		/// <returns>Size of string.</returns>
-		private static void Size(string s, string[] p, bool re) {
-			Console.WriteLine(s.Length);
-		}
-		/// <summary>
-		/// Check whether input string starts with prefix.
-		/// </summary>
-		/// <param name="s">Input string.</param>
-		/// <param name="p">prefix.</param>
-		/// <returns>1 if true, 0 otherwise.</returns>
-		private static void StartsWith(string s, string[] p, bool re) {
-			string t = Str(p, 0);
-			if(!re) Console.WriteLine(s.StartsWith(t) ? "1" : "0");
-			Match m = RegEx(t).Match(s);
-			Console.WriteLine(m.Length == t.Length ? "1" : "0");
+		/// <param name="re">NA.</param>
+		private static void LowerCase(string s, string[] p, bool re) {
+			Console.WriteLine(s.ToLower());
 		}
 		// TODO: can we make this one function for all?
 		/// <summary>
@@ -314,7 +313,7 @@ namespace orez.ostring {
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">NA.</param>
-		/// <returns>Unix lined string.</returns>
+		/// <param name="re">NA.</param>
 		private static void LfLine(string s, string[] p, bool re) {
 			Console.WriteLine(s.Replace("\r\n", "\n").Replace('\r', '\n'));
 		}
@@ -323,7 +322,7 @@ namespace orez.ostring {
 		/// </summary>
 		/// <param name="s">Input string.</param>
 		/// <param name="p">NA.</param>
-		/// <returns>Upper cased string.</returns>
+		/// <param name="re">NA.</param>
 		private static void UpperCase(string s, string[] p, bool re) {
 			Console.WriteLine(s.ToUpper());
 		}
@@ -454,7 +453,7 @@ namespace orez.ostring {
 			return v;
 		}
 
-		// 1 trim, 1 pad, ascii -> generic encodig format?
+		// 1 trim, 1 pad
 		// isint isdecimal -> can be implemented with regex equals
 		// line -> generic line find and replace?
 		// multi-line operate, input stream operate
